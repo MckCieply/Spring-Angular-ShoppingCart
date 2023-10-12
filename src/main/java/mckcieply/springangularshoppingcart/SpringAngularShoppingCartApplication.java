a7fcd0df2fad36f422bbafb927c40e53d7d25923
Alex <mwppl123@gmail.com>
2023-10-12 18:41:04 +0200

    Innit

---
+++ src/main/java/mckcieply/springangularshoppingcart/SpringAngularShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package mckcieply.springangularshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularShoppingCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularShoppingCartApplication.class, args);
+    }
+
+}
