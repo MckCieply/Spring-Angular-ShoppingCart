b6f462276b3527c90dea5bf030ae087c21e8f5d7
Alex <mwppl123@gmail.com>
2023-10-17 00:08:59 +0200

    Add delete functionality
    Allow to delete records from DB
    Enable popup confirmation
    Allow leaving popup either with confirm and abort

--- Angular-ShoppingCart/src/app/components/get-products/get-products.component.ts
+++ Angular-ShoppingCart/src/app/components/get-products/get-products.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
+import {AlertConfirmComponent} from "../../alerts/confirm/alert-confirm.component";
 
 @Component({
   selector: 'app-get-products',
@@ -8,13 +9,29 @@ import {ProductService} from "../../services/product.service";
 })
 export class GetProductsComponent implements OnInit{
   responseData: any;
-  constructor(private productService: ProductService) {
-  }
+  confirmRequired = false;
+  action="";
+  id: bigint | undefined;
+  constructor(private productService: ProductService) {}
   ngOnInit(): void {
     this.productService.getProducts().subscribe({next: (data) => {this.responseData = data}, error: console.error});
   }
 
-  onDelete(id: bigint) {
-    this.productService.deleteProduct(id).subscribe({next: console.info, error: console.error});
+  onTryDelete(id: bigint) {
+    this.confirmRequired = true;
+    this.action = "delete";
+    this.id = id;
+
+  }
+  onConfirmDelete(){
+    console.log("Deleting product with id: " + this.id)
+    if(this.id != undefined)
+      this.productService.deleteProduct(this.id).subscribe({next: console.info, error: console.error});
+    this.confirmRequired = false;
+    this.id = undefined;
+  }
+  onEdit(){
+    this.confirmRequired = true;
+    this.action = "edit";
   }
 }
